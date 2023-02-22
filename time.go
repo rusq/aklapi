4415d7f18f2af0adda82b0d06ac8e6e05fe1a5c3
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2023-02-22 20:55:58 +1000

    cleanup

--- time.go
+++ time.go
@@ -6,16 +6,6 @@ import "time"
 
 var now = time.Now // time generator
 
-// sameWeek returns true if dates t1 and t2 are on the same week
-func sameWeek(t1 time.Time, t2 time.Time) bool {
-	y1, w1 := t1.ISOWeek()
-	y2, w2 := t1.ISOWeek()
-	if y1 == y2 && w1 == w2 {
-		return true
-	}
-	return false
-}
-
 func adjustYear(t time.Time) time.Time {
 	today := now()
 	thisYear := today.Year()
