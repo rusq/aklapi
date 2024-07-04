38849b525be5cd37170b41efa3c36e4995ecb6d9
Nathan Broadbent <git@ndbroadbent.com>
2024-07-04 14:12:55 +1200

    Fix collection dates on the current day - only add a year if the date is over 7 days ago (not <= today)

--- time.go
+++ time.go
@@ -10,8 +10,8 @@ func adjustYear(t time.Time) time.Time {
 	today := now()
 	thisYear := today.Year()
 	t = t.AddDate(thisYear, 0, 0)
-	// year correction
-	if t.Before(today) && today.Format("20060102") != t.Format("20060102") {
+	// year correction if date is over a week ago
+	if t.Before(today.AddDate(0, 0, -7)) && today.Format("20060102") != t.Format("20060102") {
 		t = t.AddDate(1, 0, 0)
 	}
 	return t
